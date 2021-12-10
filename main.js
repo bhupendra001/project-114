db3ec4389b49eea837b38b8495aae8377a446229
bhupendra001 <63101397+bhupendra001@users.noreply.github.com>
2021-12-10 19:39:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload(){
+
+
+
+}
+
+function setup(){
+canvas = createCanvas(300 ,300);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+posenet = ml5.poseNet(video,modelloaded);
+posenet.on('pose' ,gotposes);
+
+}
+
+function draw(){
+
+image(video,0,0,300,300);
+
+}
+
+function snap(){
+
+save('myfilter.png')
+
+}
+
+function modelloaded(){
+
+console.log("poseNet is initialize")
+
+
+}
+
+function gotposes(result){
+
+if(result.length > 0){
+
+console.log(result)
+console.log("nose x = " + result[0].pose.nose.x);
+console.log("nose y = " + result[0].pose.nose.y);
+
+}
+
